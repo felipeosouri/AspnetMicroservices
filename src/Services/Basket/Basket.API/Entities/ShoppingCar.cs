107a3c7186ce89a37eed8f8a954adc9a23cab119
GLOBANT\juanfelipe.osorio <jfosorio@outlook.com>
2022-02-15 22:29:03 -0500

    Creating Entities - 35

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCar
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCarItem> Items { get; set; } = new List<ShoppingCarItem>();
+        public ShoppingCar()
+        {
+
+        }
+        public ShoppingCar(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
