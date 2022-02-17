41758a9c7f684e30598e1904f5ba319643f23c61
GLOBANT\juanfelipe.osorio <jfosorio@outlook.com>
2022-02-16 22:45:15 -0500

    Developing Repository Pattern on Basket.API

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCar> GetBasket(string userName);
+        Task<ShoppingCar> UpdateBasket(ShoppingCar basket);
+        Task DeleteBasket(string userName);
+    }
+}
