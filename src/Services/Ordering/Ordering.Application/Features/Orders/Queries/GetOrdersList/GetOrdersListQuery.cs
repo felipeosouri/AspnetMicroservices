61a4b4eebabf94c8d04536217d3d29bc41c92044
GLOBANT\juanfelipe.osorio <jfosorio@outlook.com>
2022-04-05 12:13:35 -0500

    CQRS Implementation with Mediator Design Pattern

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
