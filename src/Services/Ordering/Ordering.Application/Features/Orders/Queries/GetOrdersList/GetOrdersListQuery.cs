082c0f9e7c2dfd4e3a188917a0b6a3b267d8118c
GLOBANT\juanfelipe.osorio <jfosorio@outlook.com>
2022-04-05 22:30:09 -0500

    Developing Ordering.Application Features

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -1,9 +1,10 @@
 ﻿using MediatR;
 using System;
+using System.Collections.Generic;
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
 {
-    public class GetOrdersListQuery : IRequest
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
     {
         public string UserName { get; set; }
 
